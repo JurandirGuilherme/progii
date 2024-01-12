212e7709271de9db11a4b5a3b30480330c87d9b3
Jurandir Guilherme <jurandirguilherme1@gmail.com>
2024-01-12 18:17:08 -0300

    inicio

---
+++ progii/src/main/java/com/example/progii/ProgiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.progii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgiiApplication.class, args);
+	}
+
+}
---
+++ progii/src/test/java/com/example/progii/ProgiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
